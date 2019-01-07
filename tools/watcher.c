5eba84ca5215cf737cc54b7c5e7e828ca04ab496
Will Watts <will@winstonprivacy.com>
2019-01-06 21:29:23 -0600

    added sys logging

--- tools/watcher.c
+++ tools/watcher.c
@@ -24,38 +24,15 @@
 #include <getopt.h>
 
 #include <progress_ipc.h>
-
-#define RESET		0
+#include <util.h>
 
 // Function will run verification tasks on new parition
 // Currently switches state to 2 - STATE_TESTING
 static bool verification()
 {
 	system("fw_setenv ustate 2");
-	fprintf(stdout, "change ustate to 2");
+	INFO("change ustate to 2");
 	return 1; // TRUE
-}
-
-static void resetterm(void)
-{
-	fprintf(stdout, "%c[%dm", 0x1B, RESET);
-}
-
-static struct option long_options[] = {
-	{"help", no_argument, NULL, 'h'},
-	{"reboot", no_argument, NULL, 'r'},
-	{NULL, 0, NULL, 0}
-};
-
-static void usage(char *programname)
-{
-	fprintf(stdout, "%s (compiled %s)\n", programname, __DATE__);
-	fprintf(stdout, "Usage %s [OPTION]\n",
-			programname);
-	fprintf(stdout,
-		" -r, --reboot            : reboot after a successful update\n"
-		" -h, --help              : print this help and exit\n"
-		);
 }
 
 int main(int argc, char **argv)
@@ -66,43 +43,14 @@ int main(int argc, char **argv)
 	unsigned int curstep = 0;
 	char bar[60];
 	unsigned int filled_len;
-	int opt_w = 0;
-	int opt_r = 0;
-	int opt_p = 0;
 	int c;
 	RECOVERY_STATUS	status = IDLE;		/* Update Status (Running, Failure) */
 
 	/* Process options with getopt */
-	while ((c = getopt_long(argc, argv, "cwprhs:",
-				long_options, NULL)) != EOF) {
-		switch (c) {
-		case 'w':
-			opt_w = 1;
-			break;
-		case 'p':
-			opt_p = 1;
-			break;
-		case 'r':
-			opt_r = 1;
-			break;
-		case 's':
-			SOCKET_PROGRESS_PATH = strdup(optarg);
-			break;
-		case 'h':
-			usage(argv[0]);
-			exit(0);
-			break;
-		default:
-			usage(argv[0]);
-			exit(1);
-			break;
-		}
-	}
-		
+
 	tmpdir = getenv("TMPDIR");
 	if (!tmpdir)
 		tmpdir = "/tmp";
-
 	connfd = -1;
 	while (1) {
 		if (connfd < 0) {
@@ -117,68 +65,49 @@ int main(int argc, char **argv)
 		 * Something happens, show the info
 		 */
 		if ((status == IDLE) && (msg.status != IDLE)) {
-			fprintf(stdout, "\nUpdate started !\n");
-			fprintf(stdout, "Interface: ");
+			INFO("Update started !");
+			INFO("Interface: ");
 			switch (msg.source) {
-			case SOURCE_SURICATTA:
-				fprintf(stdout, "BACKEND\n\n");
-				break;
+				case SOURCE_UNKNOWN:
+					INFO("UNKNOWN");
+					break;	
+				case SOURCE_SURICATTA:
+					INFO("BACKEND");
+					break;
 			}
 
 		}
 
-		if (msg.infolen > 0)
-			fprintf(stdout, "INFO : %s\n\n", msg.info);
 
 
-		if ((msg.cur_step != curstep) && (curstep != 0))
-			fprintf(stdout, "\n");
-
-		filled_len = sizeof(bar) * msg.cur_percent / 100;
-		if (filled_len > sizeof(bar))
-			filled_len = sizeof(bar);
-
-		memset(bar,'=', filled_len);
-		memset(&bar[filled_len], '-', sizeof(bar) - filled_len);
-
-		fprintf(stdout, "[ %.60s ] %d of %d %d%% (%s)\r",
-			bar,
-			msg.cur_step, msg.nsteps, msg.cur_percent,
-		       	msg.cur_image);
-		fflush(stdout);
 
 
 		switch (msg.status) {
-		case SUCCESS:
-		case FAILURE:
-			fprintf(stdout, "\n\n");
+			case SUCCESS:
+			case FAILURE:
+				INFO("%s !", msg.status == SUCCESS ? "SUCCESS" : "FAILURE");
 
-			fprintf(stdout, "%s !\n", msg.status == SUCCESS
-							  ? "SUCCESS"
-							  : "FAILURE");
-			resetterm();
-
-			//TODO add method to verify validity of partition after msg.status SUCCESS and before reboot
-			if ((msg.status == SUCCESS)) {
-				fprintf(stdout, "SUCCESS about to verify");				
-				sleep(5);
-				if (verification()){
-					if (system("reboot") < 0) { /* It should never happen */
-						fprintf(stdout, "Please reset the board.\n");
-						system("fw_setenv ustate 3");
-						//system("/etc/init.d/swupdate restart");
+				//TODO add method to verify validity of partition after msg.status SUCCESS and before reboot
+				if ((msg.status == SUCCESS)) {
+					INFO("SUCCESS about to verify");				
+					if (verification()){
+						sleep(5);
+						if (system("reboot") < 0) { /* It should never happen */
+							fprintf(stdout, "Please reset the board.\n");
+							INFO("Please reset the board, reboot failed");
+							system("fw_setenv ustate 3");
+						}
 					}
+				} else if(msg.status == FAILURE) {
+					INFO("Change to FAILED ustate = 3");
+					system("fw_setenv ustate 3");
 				}
-			} else if(msg.status == FAILURE) {
-				fprintf(stdout, "Change to FAILED ustate = 3");		
-				system("fw_setenv ustate 3");
-			}
-			break;
-		case DONE:
-			fprintf(stdout, "\nDONE.\n");
-			break;
-		default:
-			break;
+				break;
+			case DONE:
+				INFO("DONE. ");
+				break;
+			default:
+				break;
 		}
 
 		status = msg.status;
