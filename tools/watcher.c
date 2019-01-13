1678afd14db5041b697e856c4f1b8a6aa2d12662
Will Watts <will@winstonprivacy.com>
2019-01-13 17:40:38 -0600

    added more robust u-boot routines to watcher.c

--- tools/watcher.c
+++ tools/watcher.c
@@ -8,9 +8,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <ctype.h>
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
+#include <sys/file.h>
 #include <sys/ioctl.h>
 #include <fcntl.h>
 #include <sys/types.h>
@@ -22,15 +24,23 @@
 #include <netinet/in.h>
 #include <pthread.h>
 #include <getopt.h>
+#include <dirent.h>
 #include <syslog.h>
-#include <progress_ipc.h>
-#include <util.h>
-#include <suricatta/state.h>
-#include <suricatta/suricatta.h>
+#include "generated/autoconf.h"
+#include "uboot.h"
+#include "bootloader.h"
+#include "progress_ipc.h"
+#include "util.h"
+#include "suricatta/state.h"
+#include "suricatta/suricatta.h"
 
 #define STATE_KEY "ustate"
 
 int loglevel = ERRORLEVEL;
+
+struct env_opts *fw_env_opts = &(struct env_opts) {
+	.config_file = (char *)CONFIG_UBOOT_FWENV
+};
 
 // log info function
 static void log_info(char *message){
@@ -40,7 +50,89 @@ static void log_info(char *message){
 	syslog (LOG_INFO, log_msg);
 }
 
+/*
+ * The lockfile is the same as defined in U-Boot for
+ * the fw_printenv utilities
+ */
+static const char *lockname = "/var/lock/fw_printenv.lock";
+static int lock_uboot_env(void)
+{
+	int lockfd = -1;
+	lockfd = open(lockname, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+	if (lockfd < 0) {
+		log_info("Error opening U-Boot lock file \n");
+		return -1;
+	}
+	if (flock(lockfd, LOCK_EX) < 0) {
+		log_info("Error locking file");
+		close(lockfd);
+		return -1;
+	}
 
+	return lockfd;
+}
+
+static void unlock_uboot_env(int lock)
+{
+	flock(lock, LOCK_UN);
+	close(lock);
+}
+
+static int bootloader_env_set(const char *name, const char *value)
+{
+	int lock = lock_uboot_env();
+	int ret;
+
+	if (lock < 0)
+		return -1;
+
+	if (fw_env_open (fw_env_opts)) {
+		log_info("Error: environment not initialized\n");
+		unlock_uboot_env(lock);
+		return -1;
+	}
+	fw_env_write ((char *)name, (char *)value);
+	ret = fw_env_flush(fw_env_opts);
+	fw_env_close (fw_env_opts);
+
+	unlock_uboot_env(lock);
+
+	return ret;
+}
+
+static int bootloader_env_unset(const char *name)
+{
+	return bootloader_env_set(name, "");
+}
+
+static char *bootloader_env_get(const char *name)
+{
+	int lock;
+	char *value = NULL;
+	char *var;
+
+	lock = lock_uboot_env();
+
+	if (lock < 0)
+		return NULL;
+
+	if (fw_env_open (fw_env_opts)) {
+		log_info("Error: environment not initialized\n");
+		unlock_uboot_env(lock);
+		return NULL;
+	}
+
+	var = fw_getenv((char *)name);
+
+	if (var)
+		value = strdup(var);
+
+	fw_env_close (fw_env_opts);
+
+	unlock_uboot_env(lock);
+
+	return value;
+}
 
 // Function will run verification tasks on new parition
 // Currently switches state to 2 - STATE_TESTING
@@ -103,6 +195,7 @@ int main(int argc, char **argv)
 				if ((msg.status == SUCCESS)) {
 					log_info("SUCCESS about to verify");
 					//save_state((char*)STATE_KEY, STATE_TESTING)
+					bootloader_env_set((char *)STATE_KEY, STATE_TESTING);
 					/*	
 					if ((result = save_state((char*)STATE_KEY, STATE_TESTING)) != SERVER_OK) {
 						log_info("Error while setting ustate on u-boot");
