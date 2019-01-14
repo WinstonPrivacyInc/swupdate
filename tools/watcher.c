dda338c37d747c5a40cd92e7aa7373606837094f
Will Watts <will@winstonprivacy.com>
2019-01-13 21:30:29 -0600

    .

--- tools/watcher.c
+++ tools/watcher.c
@@ -90,12 +90,9 @@ static int bootloader_env_set(const char *name, const char *value)
 		unlock_uboot_env(lock);
 		return -1;
 	}
-	fprintf(stdout, "name %s, value %s \n", name, value);
 	fw_env_write ((char *)name, (char *)value);
 	ret = fw_env_flush(fw_env_opts);
 	fw_env_close (fw_env_opts);
-
-	fprintf(stdout, "name %s, value %s \n", name, value);
 
 	unlock_uboot_env(lock);
 
